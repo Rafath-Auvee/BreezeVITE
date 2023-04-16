7fea808105e25a838ee1eb3c1a73e3dccbc2649e
Rafath-Auvee <rbza98@outlook.com>
2023-04-16 21:09:42 +0600

    DashBoard Done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello World</h1>
+            <button className="btn btn-primary">
+              <a
+                href="http://github.com/rafath-auvee"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                My Github
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
